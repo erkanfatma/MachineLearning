449d6fb6330e512255a301305be88e94b8b31c43
Fatma Erkan <erkann.fatma@gmail.com>
2021-07-07 14:12:20 +0300

    updated.

---
+++ DataAnalysis/MissingValues.py
@@ -0,0 +1,12 @@
+#Libraries
+import pandas as pd 
+
+#Load data
+datafile = pd.read_csv('missingvalues.csv')
+print(datafile)
+
+#Missing values
+
+#Numeric values
+
+#Non numeric values
--- DataAnalysis/README.md
+++ DataAnalysis/README.md
@@ -1,1 +1,10 @@
 ## Data Analysis
+
+### Data Load
+Data load operation done with pandas.
+
+```
+import pandas as pd
+
+datafile = pd.read_csv('simpledata.csv')
+```
